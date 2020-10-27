61941497c3c4ec12a6422aea5b2b96af7d46a3fb
declanChirush <hirushgimhan2016@gmail.com>
2020-10-27 14:18:22 +0530

    UI created !!!

---
+++ app/src/main/java/com/example/simple_crud_model_paper/Home.java
@@ -0,0 +1,14 @@
+package com.example.simple_crud_model_paper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
